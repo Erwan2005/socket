5ab52a50c8c9c227e0affb0c9254700fb937be19
erwan <manohisoaguyaimar@gmail.com>
2022-06-13 12:40:36 +0300

    june 19 commit

--- index.js
+++ index.js
@@ -2,16 +2,52 @@ const express = require("express");
 const app = express();
 const http = require("https");
 const cors = require("cors");
-const { io } = require("socket.io");
+const { Server } = require("socket.io");
+app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-const server = express()
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-  const io = socketIO(server);
-  io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
+const server = http.createServer(app);
+let users = [];
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  console.log("a user connected.");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
   });
-  setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("SERVER RUNNING");
+});
